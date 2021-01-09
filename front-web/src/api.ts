809603ff5a9c8decb095248b44bc6238c02c97fb
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-01-09 19:02:23 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,3 +1,4 @@
+import { OrderPayLoad } from './Orders/types';
 import axios from "axios";
 
 const API_URL = 'http://localhost:8080';
@@ -9,4 +10,8 @@ export function fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payLoad: OrderPayLoad) {
+    return axios.post(`${API_URL}/orders`, payLoad)
 }
