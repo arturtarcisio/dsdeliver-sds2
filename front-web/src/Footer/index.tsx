684b113d236f3bf41fedc972b98b018b3477ff12
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-01-09 21:14:40 -0300

    Ajustando links do footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -11,10 +11,10 @@ function Footer(){
                 <a href="https://www.youtube.com/c/DevSuperior" target="_new">
                     <YoutubeIcon/>
                 </a>
-                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                <a href="https://www.linkedin.com/in/artur-tarcisio-7382a879/" target="_new">
                     <LinkedinIcon/>
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <a href="https://www.instagram.com/arturtarcisio/" target="_new">
                     <InstagramIcon/>
                 </a>
             </div>
